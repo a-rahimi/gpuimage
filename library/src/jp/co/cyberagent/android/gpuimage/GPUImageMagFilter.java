6007362722083fbd068cf58635aa4080f564d0f5
Ali Rahimi <ali@keysduplicated.com>
2017-01-24 13:35:42 -0800

    refactor RGB->YCbCr into a function

--- library/src/jp/co/cyberagent/android/gpuimage/GPUImageMagFilter.java
+++ library/src/jp/co/cyberagent/android/gpuimage/GPUImageMagFilter.java
@@ -31,27 +31,27 @@ public class GPUImageMagFilter extends GPUImageFilter {
             " uniform float magnification;\n" +
             " uniform vec3 colorToReplace;\n" +
             " \n" +
+            " vec3 RGB_to_YCrCb(lowp vec3 rgb) {\n" +
+            "     float Y = 0.2989 * rgb.r + 0.5866 * rgb.g + 0.1145 * rgb.b;\n" +
+            "     float Cr = 0.7132 * (rgb.r - Y);\n" +
+            "     float Cb = 0.5647 * (rgb.b - Y);\n" +
+            "     return vec3(Y,Cr,Cb);\n" +
+            "  }\n" +
+            " \n" +
             " void main()\n" +
             " {\n" +
             "     lowp vec4 textureColor = texture2D(inputImageTexture, textureCoordinate);\n" +
             "     lowp vec2 magCoordinate = vec2(0.5,0.5) + (textureCoordinate-vec2(0.5,0.5)) * magnification;\n" +
             "     lowp vec4 magColor = texture2D(inputImageTexture, magCoordinate);\n" +
             "     \n" +
-            "     float maskY = 0.2989 * colorToReplace.r + 0.5866 * colorToReplace.g + 0.1145 * colorToReplace.b;\n" +
-            "     float maskCr = 0.7132 * (colorToReplace.r - maskY);\n" +
-            "     float maskCb = 0.5647 * (colorToReplace.b - maskY);\n" +
+            "     vec3 maskYCrCb = RGB_to_YCrCb(colorToReplace);\n" +
+            "     vec3 magYCrCb = RGB_to_YCrCb(magColor.rgb);\n" +
+            "     vec3 YCrCb = RGB_to_YCrCb(textureColor.rgb);\n" +
             "     \n" +
-            "     float magY = 0.2989 * magColor.r + 0.5866 * magColor.g + 0.1145 * magColor.b;\n" +
-            "     float magCr = 0.7132 * (magColor.r - magY);\n" +
-            "     float magCb = 0.5647 * (magColor.b - magY);\n" +
-            "     \n" +
-            "     float Y = 0.2989 * textureColor.r + 0.5866 * textureColor.g + 0.1145 * textureColor.b;\n" +
-            "     float Cr = 0.7132 * (textureColor.r - Y);\n" +
-            "     float Cb = 0.5647 * (textureColor.b - Y);\n" +
-            "     lowp float my_distance = distance(vec2(Cr, Cb), vec2(maskCr, maskCb));\n" +
+            "     float my_distance = distance(vec2(YCrCb.y, YCrCb.z), vec2(maskYCrCb.y, maskYCrCb.z));\n" +
             "     textureColor *= step(thresholdSensitivity, my_distance);\n" +
             "     \n" +
-            "     lowp float mag_distance = distance(vec2(magCr, magCb), vec2(maskCr, maskCb));\n" +
+            "     lowp float mag_distance = distance(vec2(magYCrCb.y, magYCrCb.z), vec2(maskYCrCb.y, maskYCrCb.z));\n" +
             "     lowp float blendValue = 1.0 - smoothstep(thresholdSensitivity, thresholdSensitivity + smoothing, mag_distance);\n" +
             "     gl_FragColor = mix(textureColor, magColor, blendValue*0.7);\n" +
             " }";
